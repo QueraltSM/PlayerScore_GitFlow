94dc0b23e99829049c80069f2ed9ec79510fa9ea
Queralt Sosa Mompel <queraltsosa@hotmail.com>
2018-10-28 22:06:26 +0000

    Added one more test with test3_branch, a feature branch

--- src/main/java/PlayerScore.java
+++ src/main/java/PlayerScore.java
@@ -8,12 +8,33 @@ public class PlayerScore {
         this.rolls = new ArrayList<Integer>();
     }
 
-    public int frames() {
-        return rolls.size();
+    public List<Frame> frames() {
+        List<Frame> frames = new ArrayList<Frame>();
+        for (int i = 0; i<rolls.size() && frames.size()<10; i += new Frame(i).isStrike() ? 1 : 2) frames.add(new Frame(i));
+        return frames;
     }
 
     public PlayerScore roll(int pins) {
         rolls.add(pins);
         return this;
+    }
+    public class Frame {
+        private int offset;
+
+        public Frame(int offset) {
+            this.offset = offset;
+        }
+
+        public boolean isStrike() {
+            return roll(0) == 10;
+        }
+
+        private Integer roll(int i) {
+            return isOutOfBounds(i) ? rolls.get(i + offset) : 0;
+        }
+
+        private boolean isOutOfBounds(int i) {
+            return i < rolls.size() - offset;
+        }
     }
 }
--- src/test/java/PlayerScore_GitFlow_.java
+++ src/test/java/PlayerScore_GitFlow_.java
@@ -5,12 +5,18 @@ public class PlayerScore_GitFlow_ {
 
     @Test
     public void given_new_player_score_frames_should_be_zero() {
-        assert(playerScore.frames()==0);
+        assert(playerScore.frames().size()==0);
     }
 
     @Test
     public void given_one_roll_frames_should_be_one() {
         playerScore.roll(4);
-        assert(playerScore.frames()==1);
+        assert(playerScore.frames().size()==1);
+    }
+
+    @Test
+    public void given_two_rolls_frame_score_should_be_the_sum_of_them() {
+        playerScore.roll(4).roll(3);
+        assert(playerScore.frames().size()==1);
     }
 }
